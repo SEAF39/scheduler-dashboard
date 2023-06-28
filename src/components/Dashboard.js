749c2ecc3f1163069f856b48fdcd3d0feeb87a3b
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-27 23:11:04 -0400

    Loading Component & Panel Component & Handling State

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,81 @@
-import React, { Component } from "react";
-
+/* Dashboard.js */import React, { Component } from "react";
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  constructor(props) {
+    super(props);
 
-    return <main className={dashboardClasses} />;
+    this.state = {
+      loading: false,
+      focused: null
+    };
+
+    this.selectPanel = this.selectPanel.bind(this);
+  }
+
+  selectPanel(id) {
+    this.setState((prevState) => ({
+      focused: prevState.focused !== null ? null : id
+    }));
+  }
+
+  render() {
+    const { loading, focused } = this.state;
+
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": focused
+    });
+
+    if (loading) {
+      return <Loading />;
+    }
+
+    const panels = focused
+      ? data
+          .filter((panel) => panel.id === focused)
+          .map((panel) => (
+            <Panel
+              key={panel.id}
+              label={panel.label}
+              value={panel.value}
+              onSelect={this.selectPanel}
+            />
+          ))
+      : data.map((panel) => (
+          <Panel
+            key={panel.id}
+            label={panel.label}
+            value={panel.value}
+            onSelect={this.selectPanel}
+          />
+        ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+/* Loading.js */
+
+import React, { Component } from "react";
+
+export default class Loading extends Component {
+  render() {
+    return <section className="loading">Loading</section>;
+  }
+}
